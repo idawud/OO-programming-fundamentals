e408239d04e355066af41edead7bd7315a216bf3
idawud <ismaildawud96@gmail.com>
2019-10-18 15:34:08 +0000

    date status checker

---
+++ 4.conditional/date.C
@@ -0,0 +1,51 @@
+#include <iostream>
+
+struct Date{
+	int year;
+	int month;
+	int day;
+};
+
+
+
+void dateStatus(Date date1, Date date2){
+	if( date1.year > date2.year){ 
+		std::cout << "Before \n";
+	}
+	else if ( date1.year < date2.year) { 
+		std::cout << "After\n";
+	}
+	else{
+		if( date1.month > date2.month){
+			std::cout << "Before\n";
+		}
+		else if ( date1.month < date2.month) { 
+			std::cout << "After\n";
+		}
+		else{
+			if( date1.day > date2.day){
+				std::cout << "Before\n";
+			}
+			else if ( date2.day > date1.day) {
+				std::cout << "After\n";
+			 }
+			else{
+				std::cout << "Current Date\n";
+			}
+		}
+	}
+}
+
+	
+int main(){
+	Date my_birthday {1996,4,25};
+	
+	Date date {};
+
+	std::cout << "Enter a year, month and day: ";
+	std::cin >> date.year >> date.month >> date.day;
+	
+
+	dateStatus(my_birthday, date);
+
+}
