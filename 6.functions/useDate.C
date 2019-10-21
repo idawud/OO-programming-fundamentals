cf1bb6f0663ad18c8d7b241a0a44e381e3361aba
idawud <ismaildawud96@gmail.com>
2019-10-21 21:41:59 +0000

    labs completion for func

--- 6.functions/useDate.C
+++ 6.functions/useDate.C
@@ -24,9 +24,42 @@ int main() {
 
 	prettyprint(max(date1, date2));
 
+	// Problem 4
+	setDateToJanNextYear(&date2);
+	prettyprint(date2);
+
+	// Problem 5
 	swapDates(&date1, &date2);
 	prettyprint(date1);
 
-	setDateToJanNextYear(&date2);
-	prettyprint(date2);
+	// Problem 6
+	Date date3 {};
+	date3.year = 1928;
+	date3.month = 8;
+	date3.day = 12;
+	
+	Date date4 {};
+	date4.year = 2019;
+	date4.month = 10;
+	date4.day = 21;
+
+	Date *ptrDt1 { &date1 };
+	Date *ptrDt2 { &date2 };
+	Date *ptrDt3 { &date3 };
+	Date *ptrDt4 { &date4 };
+
+	std::cout << "\nDirect Addressess: " << std::endl;
+	std::cout << "Address d1: " << ptrDt1 << "\nAddress d2: " << ptrDt2 << "\nAddress d3: " << ptrDt3 << "\nAddress d4: " << ptrDt4 << std::endl;
+
+	std::cout << "\nUsing Arrays: " << std::endl;
+	Date dateArr [4] { date1, date2, date3, date4 };
+	for (int i = 0; i < 4; ++i){
+		std::cout << "Address: " << &dateArr[i] << std::endl;
+	}
+
+	std::cout << "\nUsing Vectors: " << std::endl;
+	std::vector<Date> dates { date1, date2, date3, date4 };
+	for (int i = 0; i < 4; ++i){
+		std::cout << "Address: " << &dates[i] << std::endl;
+	}
 }
