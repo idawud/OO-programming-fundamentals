6de2a08b601c9c2cad10962f3fb049c4e8fda7ce
idawud <ismaildawud96@gmail.com>
2019-10-23 12:23:23 +0000

    removed redundant parameter for printMatrix()

--- 7.arrays/array.C
+++ 7.arrays/array.C
@@ -1,8 +1,8 @@
 #include <iostream>
 
-void printMatrix(int matrix [][3], int n, int m){
+void printMatrix(int matrix [][2], int n){
 	for( int i = 0; i < n; ++i){
-		for(int j = 0; j < m; ++j){
+		for(int j = 0; j < 2; ++j){
 			std::cout << matrix[i][j] << " ";
 		}
 		std::cout << std::endl;
@@ -10,10 +10,10 @@ void printMatrix(int matrix [][3], int n, int m){
 }
 
 
-void printMatrixThreeDim (int matrix [][2][3], int n, int m, int o){
+void printMatrixThreeDim (int matrix [][2][3], int n){
 	for( int i = 0; i < n; ++i){
-		for(int j = 0; j < m; ++j){
-			for(int k = 0; k < o; ++k){
+		for(int j = 0; j < 2; ++j){
+			for(int k = 0; k < 3; ++k){
 				std::cout << matrix[i][j][k] << " ";
 			}
 			std::cout << std::endl;
@@ -51,18 +51,18 @@ int main(){
 	std::cout << arr[1] << std::endl;
 
 	// Problem 3
-	int matrix[2][3] { 1, 2, 3, 4, 5, 6};
-	printMatrix(matrix, 2, 3);
+	int matrix[2][2] { 1, 2, 3, 4};
+	printMatrix(matrix, 2);
 	
-	std::cout << "Three dimentional array" << std::endl;
+	std::cout << "\nThree dimentional array" << std::endl;
 	int mat [2][2][3] { 1,2,3,4,5,6,7,8,9,10,11,12 };
-	printMatrixThreeDim(mat,2,2,3);
+	printMatrixThreeDim(mat,2);
 
 	//Problem 4
 	char turntabl[] {'t', 'u', 'r', 'n', 't','a', 'b','l'};
 	char dawud[] {'d', 'a', 'w', 'u', 'd'};
 
-    	auto result = longestChar( turntabl, dawud);
+    	auto result = longestChar( turntabl,dawud);
     	std::cout << "The longest char * is: " <<  result << "\n";	
 
 }
